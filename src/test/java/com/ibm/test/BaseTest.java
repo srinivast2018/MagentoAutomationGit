49b4f195d2e728798b2d3bc54972712fb93724ee
srinivast2018 <srinivas.nirmal@gmail.com>
2019-01-04 15:33:17 +0530

    fitstcommit

---
+++ src/test/java/com/ibm/test/BaseTest.java
@@ -0,0 +1,15 @@
+package com.ibm.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BaseTest {
+	
+	@Test
+	public void LoginTest()
+	{
+		System.out.println("chek");
+		Assert.assertEquals("myaccount","myaccount");
+	}
+
+}
